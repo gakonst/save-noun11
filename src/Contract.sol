d0bc108c27b69c421defaba5de9dd0a467fdbd63
Georgios Konstantopoulos <me@gakonst.com>
2022-05-05 16:50:55 -0700

    feat: add fractional market wrapper

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,68 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.9;
 
-contract Contract {}
+import "partybid/market-wrapper/IMarketWrapper.sol";
+
+// The fractional factory
+interface FractionalFactory {
+    function vaults(uint256) external view returns (FractionalVault);
+}
+
+// The state of an auction
+enum State { inactive, live, ended, redeemed }
+
+// A specific fractional auction
+interface FractionalVault {
+    function auctionState() external view returns (State);
+    function end() external;
+    function bid() payable external;
+    function winning() external view returns (address);
+    function livePrice() external view returns (uint256);
+}
+
+interface FractionalSettings {
+    function minBidIncrease() external view returns (uint256);
+}
+
+// WETH
+// address public constant weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+
+contract FractionalMarketWrapper is IMarketWrapper {
+    // https://github.com/fractional-company/contracts#mainnet
+    FractionalFactory public constant factory = FractionalFactory(0x85Aa7f78BdB2DE8F3e0c0010d99AD5853fFcfC63);
+    FractionalSettings public constant settings = FractionalSettings(0xE0FC79183a22106229B84ECDd55cA017A07eddCa);
+
+    // if the contract has any WETH, unwrap it into ETH?
+    function bid(uint256 auctionId, uint256 bidAmount) external {
+        factory.vaults(auctionId).bid{value: bidAmount}();
+    }
+
+    function finalize(uint256 auctionId) external override {
+        factory.vaults(auctionId).end();
+    }
+
+    function getCurrentHighestBidder(uint256 auctionId) external view override returns (address) {
+        return factory.vaults(auctionId).winning();
+    }
+
+    function isFinalized(uint256 auctionId) external view override returns (bool) {
+        return factory.vaults(auctionId).auctionState() == State.ended;
+    }
+
+    function getMinimumBid(uint256 auctionId) external view override returns (uint256) {
+        // bump the price a little bit
+        uint256 pctIncrease = settings.minBidIncrease() + 1000;
+        uint256 price = factory.vaults(auctionId).livePrice();
+        // divide by 1000 to re-normalize the pct
+        return price * pctIncrease / 1000;
+    }
+
+    function auctionIdMatchesToken(
+        uint256 auctionId,
+        // unused params
+        address, uint256
+    ) public view override returns (bool) {
+        return address(factory.vaults(auctionId)) != address(0x0);
+    }
+}
