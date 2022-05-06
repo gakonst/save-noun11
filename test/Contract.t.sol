568717ee2c6a095aa98af247252f3d590fb092a1
Georgios Konstantopoulos <me@gakonst.com>
2022-05-05 18:06:46 -0700

    test: set the correct auction id

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -8,7 +8,7 @@ import "partybid/PartyBid.sol";
 import "partybid/PartyBidFactory.sol";
 
 interface Nouns {
-
+    function ownerOf(uint256) external view returns (address);
 }
 
 contract ContractTest is Test {
@@ -23,6 +23,8 @@ contract ContractTest is Test {
 
     // the noun we're testing with
     uint256 constant noun11 = 11;
+    // https://etherscan.io/tx/0x14292770d0867c9d78234a11a7d5afe558dab4acf7269eb600674a034c54c350#eventlog
+    uint256 constant noun11Vault = 275;
     // the market wrapper creator wants some rent
     address constant rentAddress = address(0x1234);
     uint256 constant rentBasisPoints = 200;
@@ -37,7 +39,7 @@ contract ContractTest is Test {
             address(nouns), // nftcontract
             // these can be the same for Fractional, given that we go via the factory
             noun11, // tokenId
-            noun11, // auctionId
+            noun11Vault, // auctionId
             Structs.AddressAndAmount({ addr: rentAddress, amount: rentBasisPoints }),
             Structs.AddressAndAmount({ addr: address(0), amount: 0 }),
             "Noun11",
@@ -47,8 +49,22 @@ contract ContractTest is Test {
     }
 
     function testCanSaveNoun11() public {
-        // TODO
+        // some people contribute to the auction
+        bid.contribute{value: 200 ether}();
+
+        // bid!
+        bid.bid();
+
+        // move the time to the end of the auction so we can finalize
+        FractionalVault vault = fractional.factory().vaults(noun11Vault);
+        uint256 endTime = vault.auctionEnd();
+        vm.warp(endTime);
+
+        // wrap it up
+        bid.finalize();
+
+        // assertEq(nouns.ownerOf(noun11, address(bid));
     }
 
-    // TODO: Test refunds
+    // TODO: Test refunds with WETH edge case
 }
